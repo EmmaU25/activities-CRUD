1fd7ab6fd70235b02c3627aa0d19e41896b217b6
Emmanuel <eyah@cbxfmarketplace.com>
2020-11-03 14:38:54 -0600

    init project with page declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { WeatherComponent } from './pages/weather/weather.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivitiesComponent,
+    ChartsComponent,
+    WeatherComponent,
+    GalleryComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
