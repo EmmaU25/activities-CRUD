df45dc3ab2d5d15085e65ce1273bd1766d4c4da5
Emmanuel <eyah@cbxfmarketplace.com>
2020-11-05 12:59:08 -0600

    Add pipes and components module for header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActivitiesComponent } from './pages/activities/activities.component';
@@ -9,6 +9,8 @@ import { WeatherComponent } from './pages/weather/weather.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {ComponentsModule} from 'src/app/components/components.module';
+import { CapitalLetterPipe } from './pipes/capital-letter.pipe';
 
 @NgModule({
   declarations: [
@@ -17,12 +19,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ChartsComponent,
     WeatherComponent,
     GalleryComponent,
-    DetailsComponent
+    DetailsComponent,
+    CapitalLetterPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ComponentsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
